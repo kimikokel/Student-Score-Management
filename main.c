558f21e9490b0e7e8ebb1442854d45170e42ed16
kimikokel <kellyleong2002@gmail.com>
2022-12-29 03:24:52 +0800

    struct code

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct user{
+    char name[50];
+    int score[100];
+}student;
+
+typedef struct Node{
+    student std;
+    struct Node* next;
+}node;
+
+node *head = NULL;
+
+void adduser(student name, student score){
+    
+}
+
+void InputStudent() {
+	node* NewNode = (node*)malloc(sizeof(node));
+	NewNode->next = NULL;
+
+	node* p = head;
+	while (head != NULL && p->next != NULL)
+	{
+		p = p->next;
+	}
+
+	if (head == NULL) {
+		head = NewNode;
+	}
+	else {
+		p->next = NewNode;
+	}
+
+	printf("请输入学生姓名：\n");
+	scanf("%s %d %d %d %d %d", NewNode->std.name, &NewNode->std.score[0], &NewNode->std.score[1], &NewNode->std.score[2], &NewNode->std.score[3], &NewNode->std.score[4]);
+
+	printf("学生信息录入成功。\n\n");
+	system("pause");
+	system("cls");
+}
+
+int main(){
+    return 0;
+}
