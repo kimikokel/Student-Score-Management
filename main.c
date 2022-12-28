27bdaa63ef11686ad694ba95d4a5e290f6d906de
kimikokel <kellyleong2002@gmail.com>
2022-12-29 04:29:42 +0800

    added num_exe, modified add_user

--- main.c
+++ main.c
@@ -14,6 +14,14 @@ typedef struct Node{
 
 node *head = NULL;
 
+int num_exe(){
+	//how many executions
+	printf("How many students are there?\n");
+	int buff;
+	scanf("%d",buff);
+	return buff;
+}
+
 void adduser() {
 	node* NewNode = (node*)malloc(sizeof(node));
 	NewNode->next = NULL;
@@ -33,11 +41,17 @@ void adduser() {
 
 	printf("Enter student name and scores:\n");
 	scanf("%s %d %d %d %d %d", NewNode->std.name, &NewNode->std.score[0], &NewNode->std.score[1], &NewNode->std.score[2], &NewNode->std.score[3], &NewNode->std.score[4]);
+	//&NewNode->std.score->next
 
 	printf("%s %d %d %d %d %d\n\n", NewNode->std.name, &NewNode->std.score[0], &NewNode->std.score[1], &NewNode->std.score[2], &NewNode->std.score[3], &NewNode->std.score[4]);
+	printf("Success...\n\n");
 }
 
 int main(){
+	// int loops = num_exe();
 	adduser();
+	// for(int i=0; i<loops; i++){
+	// 	adduser();
+	// }
     return 0;
 }
